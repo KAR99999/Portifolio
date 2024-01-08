10fb355973013dd435b55553f9950d1e15f2f4d7
Muhammed Kar <muhammedkar989@gmail.com>
2024-01-08 15:09:42 +0300

    addin alpha about me sectrion

--- portifolio/src/app/app.component.ts
+++ portifolio/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent,NavbarComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent,NavbarComponent,AboutComponent]
 })
 export class AppComponent {
   title = 'portifolio';
