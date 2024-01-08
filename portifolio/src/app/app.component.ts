4f9102526ee826885d85f60c630c10eb0741a4e9
Muhammed Kar <muhammedkar989@gmail.com>
2024-01-08 14:38:06 +0300

    adding alfa first section

--- portifolio/src/app/app.component.ts
+++ portifolio/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent,NavbarComponent]
 })
 export class AppComponent {
   title = 'portifolio';
